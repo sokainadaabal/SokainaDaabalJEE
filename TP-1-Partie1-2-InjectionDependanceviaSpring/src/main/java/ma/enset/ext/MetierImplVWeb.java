c9a29444c3a2dcd1fc32b4849e1fdad6cdf6e777
sokainadaabal <48890714+sokainadaabal@users.noreply.github.com>
2023-02-13 11:35:49 +0100

    ajouter partie injection dependance via Spring

---
+++ TP-1-Partie1-2-InjectionDependanceviaSpring/src/main/java/ma/enset/ext/MetierImplVWeb.java
@@ -0,0 +1,17 @@
+package ma.enset.ext;
+
+import ma.enset.dao.IDao;
+import ma.enset.metier.Imetier;
+
+public class MetierImplVWeb implements Imetier {
+    IDao dao =null;
+    @Override
+    public double calcul() {
+        System.out.println("Version web Service");
+        return 203;
+    }
+
+    public void setDao(IDao dao) {
+        this.dao = dao;
+    }
+}
