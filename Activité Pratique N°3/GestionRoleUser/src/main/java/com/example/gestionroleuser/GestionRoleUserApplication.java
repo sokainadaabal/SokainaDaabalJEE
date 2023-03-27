5ba00640d2bdff1031676ef072cf4f3da6e73512
sokainadaabal <48890714+sokainadaabal@users.noreply.github.com>
2023-03-27 04:53:33 +0000

    update

--- Activité Pratique N°3/GestionRoleUser/src/main/java/com/example/gestionroleuser/GestionRoleUserApplication.java
+++ Activité Pratique N°3/GestionRoleUser/src/main/java/com/example/gestionroleuser/GestionRoleUserApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GestionRoleUserApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GestionRoleUserApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GestionRoleUserApplication.class, args);
+    }
 
 }
--- Activité Pratique N°3/getion-patient/src/main/java/ma/enset/getionpatient/GetionPatientApplication.java
+++ Activité Pratique N°3/getion-patient/src/main/java/ma/enset/getionpatient/GetionPatientApplication.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 import java.util.Date;
+import java.util.List;
 
 @SpringBootApplication
 public class GetionPatientApplication implements CommandLineRunner {
@@ -20,6 +23,9 @@ public class GetionPatientApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        /**
+         * add a new patient
+         */
         patientRepository.save(new Patient(null,"Sokaina",new Date(),false,56));
         patientRepository.save(new Patient(null,"Saad",new Date(),true,56));
         patientRepository.save(new Patient(null,"Ahmed saad",new Date(),true,56));
@@ -27,7 +33,9 @@ public class GetionPatientApplication implements CommandLineRunner {
         patientRepository.findAll().forEach(p->{
             System.out.println(p);
         });
-
+        /**
+         * search all patient
+         */
         System.out.println("-----------------------------");
         for (Patient p: patientRepository.findAll()
              ) {
@@ -39,6 +47,51 @@ public class GetionPatientApplication implements CommandLineRunner {
             System.out.println("<=============>");
         }
         System.out.println("-----------------------------");
+        /**
+         * search a patient
+         */
+         Patient patient = patientRepository.findById(2L).orElse(null);
+        if(patient!=null){
+                System.out.println(patient.getId());
+                System.out.println(patient.getNom());
+                System.out.println(patient.getDateNaissance());
+                System.out.println(patient.getScore());
+                System.out.println(patient.isMalade());
+        }
+        System.out.println("-----------------------------");
+         /**
+         * update a patient
+         */
+         patient.setScore(790);
+        /**
+         * delete patient
+         */
+        patientRepository.deleteById(1l);
+        for (int i = 0; i < 100; i++) {
+            patientRepository.save(new Patient(null, "Mohammed",
+                    new Date(), false, 56));
+        }
+
+        for (Patient patient1 : patientRepository.findAll(PageRequest.of(0, 5))) {
+            System.out.println(patient1.getId());
+            System.out.println(patient1.getNom());
+            System.out.println(patient1.getScore());
+            System.out.println(patient1.getDateNaissance());
+            System.out.println(patient1.isMalade());
+            System.out.println("=========");
+        }
+
+        Page<Patient> pages = patientRepository.findAll(PageRequest.of(0, 5));
+        pages.getTotalElements();
+        pages.getTotalPages();
+        pages.getNumber();
+        List<Patient> patientList = pages.getContent();
+
+
+        List<Patient> list = patientRepository.findByMalade(false);
+
+        Page<Patient> list2 = patientRepository.findByMalade(false, PageRequest.of(0, 5));
+
 
     }
 }
