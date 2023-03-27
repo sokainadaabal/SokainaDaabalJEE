59ce7880ca6a6441f448a36249c0b87bdef41c84
sokainadaabal <48890714+sokainadaabal@users.noreply.github.com>
2023-03-27 01:36:50 +0000

    add new activitie

---
+++ Activité Pratique N°3/getion-patient/src/main/java/ma/enset/getionpatient/GetionPatientApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.getionpatient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetionPatientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetionPatientApplication.class, args);
+	}
+
+}
