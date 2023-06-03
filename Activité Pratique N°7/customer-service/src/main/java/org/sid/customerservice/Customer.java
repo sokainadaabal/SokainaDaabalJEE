e492649b512d998bfa6ee5120e3862d4bc0caea1
sokainadaabal <48890714+sokainadaabal@users.noreply.github.com>
2023-06-03 17:07:13 +0100

    update

--- Activité Pratique N°7/customer-service/src/main/java/org/sid/customerservice/Customer.java
+++
@@ -1,4 +0,0 @@
-package org.sid.customerservice;
-
-public class Customer {
-}
--- Activité Pratique N°7/customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
+++ Activité Pratique N°7/customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
@@ -1,7 +1,11 @@
 package org.sid.customerservice;
 
+import org.sid.customerservice.entites.Customer;
+import org.sid.customerservice.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
@@ -10,4 +14,16 @@ public class CustomerServiceApplication {
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner start(CustomerRepository customerRepository){
+		return args -> {
+			customerRepository.save(new Customer(null,"saad","saadbelfquih@gmail.com"));
+			customerRepository.save(new Customer(null,"ahmed saad","ahmedsaadbelfquih@gmail.com"));
+			customerRepository.save(new Customer(null,"mohammed saad","mohammedsaadbelfquih@gmail.com"));
+			customerRepository.findAll().forEach(customer -> {
+				System.out.println(customer.toString());
+			});
+
+		};
+	}
 }
